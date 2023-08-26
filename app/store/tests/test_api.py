0e41d4d0589a2128177b298ef5392610debab0fa
Oleg Remizov <job.rem1zoff@gmail.com>
2023-08-26 18:05:38 +0300

    backend filters test

--- app/store/tests/test_api.py
+++ app/store/tests/test_api.py
@@ -6,17 +6,35 @@ from store.serializers import BooksSerializer
 
 class BooksApiTestCase(APITestCase):
 
-    def test_get(self):
-        self.book_1 = Book.objects.create(name='Harry Potter', price=1000.00)
-        self.book_2 = Book.objects.create(name='The Wichter', price=700.00)
+    def setUp(self):
+        self.book_1 = Book.objects.create(name='Harry Potter', price=1000.00, author='J. K. Rowling')
+        self.book_2 = Book.objects.create(name='The Witcher', price=700.00, author='Andrzej Sapkowski')
+        self.book_3 = Book.objects.create(name='The lord of the Rowling', price=900.00, author='Tolkien')
 
+    def test_get(self):
         # url = reverse('book-list')
         url = '/book/'  # 'http://127.0.0.1:8000/book/'
         response = self.client.get(url)
-        serializer_data  = BooksSerializer([self.book_1, self.book_2], many=True).data
+        serializer_data  = BooksSerializer([self.book_1, self.book_2, self.book_3], many=True).data
 
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
 
-        print(response.data)
+    def test_get_search(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Rowling'})
+        serializer_data  = BooksSerializer([self.book_1, self.book_3], many=True).data
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+
+
+    def test_get_ordering(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'ordering': 'price'})
+        serializer_data  = BooksSerializer([self.book_2, self.book_3, self.book_1], many=True).data
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
--- app/store/tests/test_serializers.py
+++ app/store/tests/test_serializers.py
@@ -6,23 +6,33 @@ from collections import OrderedDict
 
 class BookSerializerTestCase(TestCase):
     def setUp(self):
-        self.book_1 = Book.objects.create(name='Harry Potter', price=1000.00)
-        self.book_2 = Book.objects.create(name='The Witcher', price=700.00)
+        self.book_1 = Book.objects.create(name='Harry Potter', price=1000.00, author='User1')
+        self.book_2 = Book.objects.create(name='The Witcher', price=700.00, author='User2')
+        self.book_3 = Book.objects.create(name='The lord of the rings', price=900.00, author='User3')
 
     def test_ok(self):
 
-        data = BooksSerializer([self.book_1, self.book_2], many=True).data
+        data = BooksSerializer([self.book_1, self.book_2, self.book_3], many=True).data
         expected_result = [
             {
                 "id": self.book_1.id,
                 "name": "Harry Potter",
-                "price": "1000.00"
+                "price": "1000.00",
+                'author': 'User1',
             },
             {
                 "id": self.book_2.id,
                 "name": "The Witcher",
-                "price": "700.00"
+                "price": "700.00",
+                'author': 'User2',
+            },
+                        {
+                "id": self.book_3.id,
+                "name": "The lord of the rings",
+                "price": "900.00",
+                'author': 'User3',
             }
         ]
         self.assertEqual(expected_result, data)
+
 

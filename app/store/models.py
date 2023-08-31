7efd2f8fa82aebf4c772e30957e5975450f8c8fc
Oleg Remizov <job.rem1zoff@gmail.com>
2023-08-31 12:06:33 +0300

    new model money_to_many  "like and rating"

--- app/store/models.py
+++ app/store/models.py
@@ -7,7 +7,33 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')
+    
     def __str__(self) -> str:
         return f'id: {self.id} | {self.name}'
+    
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Ok'),
+        (2, 'Fine'),
+        (3, 'Good'),
+        (4, 'Amazing'),
+        (5, 'Incredible'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} | {self.book.name} | {self.rate}'
+    
+# Залайканные или имеют рейтинг | related_name='books' | user.books.all(): 
+# <QuerySet [<Book: id: 5 | Страж>, <Book: id: 1 | Harry Potter and the philosopher']>
+
+# Owner | related_name='my_books' | user.my_books.all():
+# <QuerySet [<Book: id: 1 | Harry Potter and the philosopher's stone>, <Book: id: 2 | The Witcher>]>

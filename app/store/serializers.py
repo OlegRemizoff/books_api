c8f06863a383fea7cd9af8bcccf3d23a2d17540c
Oleg Remizov <job.rem1zoff@gmail.com>
2023-09-03 16:01:39 +0300

    likes_count through SerializerMethodField()

--- app/store/serializers.py
+++ app/store/serializers.py
@@ -3,10 +3,15 @@ from .models import Book, UserBookRelation
 
 
 class BooksSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'name', 'price', 'author', 'likes_count')
 
+    def get_likes_count(self, instance):
+        return UserBookRelation.objects.filter(book=instance, like=True).count() # instance текущая книга
+    
 
 class UserBookRelationSerializer(serializers.ModelSerializer):
     class Meta:
--- app/store/tests/test_serializers.py
+++ app/store/tests/test_serializers.py
@@ -1,19 +1,31 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from store.serializers import BooksSerializer
-from store.models import Book
-from collections import OrderedDict
+from store.models import Book, UserBookRelation
+
 
 
 class BookSerializerTestCase(TestCase):
+    # ./manage.py test store.tests.test_serializers.BookSerializerTestCase
     def setUp(self):
-        self.user = User.objects.create(username='test_user')
+        self.user1 = User.objects.create(username='user1')
+        self.user2 = User.objects.create(username='user2')
+        self.user3 = User.objects.create(username='user3')
+
         self.book_1 = Book.objects.create(name='Harry Potter', price=1000.00, 
-                                          author='User1', owner=self.user)
+                                          author=self.user1)
         self.book_2 = Book.objects.create(name='The Witcher', price=700.00,
-                                          author='User2', owner=self.user)
+                                          author=self.user2)
         self.book_3 = Book.objects.create(name='The lord of the rings', price=900.00, 
-                                          author='User3', owner=self.user)
+                                          author=self.user3)
+
+        UserBookRelation.objects.create(user=self.user1, book=self.book_1, like=True)
+        UserBookRelation.objects.create(user=self.user2, book=self.book_1, like=True)
+        UserBookRelation.objects.create(user=self.user3, book=self.book_1, like=True)
+
+        UserBookRelation.objects.create(user=self.user1, book=self.book_2, like=True)
+        UserBookRelation.objects.create(user=self.user2, book=self.book_2, like=True)
+        UserBookRelation.objects.create(user=self.user3, book=self.book_2, like=False)
 
     def test_ok(self):
 
@@ -23,25 +35,28 @@ class BookSerializerTestCase(TestCase):
                 "id": self.book_1.id,
                 "name": "Harry Potter",
                 "price": "1000.00",
-                'author': 'User1',
-                'owner': self.user.id,
+                'author': self.user1.username,
+                'likes_count': 3,
             },
             {
                 "id": self.book_2.id,
                 "name": "The Witcher",
                 "price": "700.00",
-                'author': 'User2',
-                'owner': self.user.id,
+                'author': self.user2.username,
+                'likes_count': 2,
             },
                         {
                 "id": self.book_3.id,
                 "name": "The lord of the rings",
                 "price": "900.00",
-                'author': 'User3',
-                'owner': self.user.id,
+                'author': self.user3.username,
+                'likes_count': 0,
             }
         ]
 
+        print(expected_result)
+        print('================')
+        print(data)
         self.assertEqual(expected_result, data)
 
 
